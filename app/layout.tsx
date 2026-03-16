f219523c2dc73bf693997d04b5e0dbfdc0f66584
김도영 <zz01064842675@gmail.com>
2026-03-16 09:03:01 +0900

    fix: favicon 파일 형식을 PNG로 교체
    - `/favicon-32x32` 및 `/favicon-16x16` 파일 형식을 `.svg`에서 `.png`로 수정
    - Web App Manifest 관련 경로 및 속성 업데이트

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,10 +39,10 @@ export const metadata: Metadata = {
   },
   manifest: "/manifest.webmanifest",
   icons: {
-    shortcut: [{ url: "/favicon-32x32.svg", type: "image/svg+xml" }],
+    shortcut: [{ url: "/favicon-32x32.png", type: "image/svg+xml" }],
     icon: [
-      { url: "/favicon-32x32.svg", type: "image/svg+xml", sizes: "32x32" },
-      { url: "/favicon-16x16.svg", type: "image/svg+xml", sizes: "16x16" },
+      { url: "/favicon-32x32.png", type: "image/svg+xml", sizes: "32x32" },
+      { url: "/favicon-16x16.png", type: "image/svg+xml", sizes: "16x16" },
     ],
     apple: [{ url: "/apple-touch-icon.png", sizes: "180x180" }],
   },
